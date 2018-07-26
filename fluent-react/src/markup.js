ccfae88a413c90052d0ec5807f7d8c32ae7942fb
Mark Weaver <mark@blushingpenguin.com>
2018-07-26 15:21:32 +0000

    Revert "Merge branch 'jsdompeer' into vendeq"
    This reverts commit 86ab7fe5182a6adb40e498a776ea0a33dcf96d66, reversing
    changes made to 601322dc68809ed746e36db4206753b6ff380b1d.

--- fluent-react/src/markup.js
+++ fluent-react/src/markup.js
@@ -1,12 +1,6 @@
-/* global window, require */
-let doc = null;
-if (typeof(window) === "undefined" || !window || !window.document) {
-  const jsdom = require("jsdom");
-  doc = new jsdom.JSDOM("<html><body></body></html>").window.document;
-} else {
-  doc = window.document;
-}
-const TEMPLATE = doc.createElement("template");
+/* eslint-env browser */
+
+const TEMPLATE = document.createElement("template");
 
 export function parseMarkup(str) {
   TEMPLATE.innerHTML = str;
